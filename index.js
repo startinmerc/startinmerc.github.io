24e02d342b92a0e5362119f5a703ad82cc03b1c1
STM <startinmerc@gmail.com>
2020-06-29 14:14:08 +0100

    Indentation

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const colors = [
 
 // Returns random color class name
 function getRandomColor() {
-  return colors[Math.floor(Math.random() * colors.length)];
+	return colors[Math.floor(Math.random() * colors.length)];
 }
 
 // Split chars/words
@@ -55,34 +55,34 @@ const heroDuration = 2.2
 
 // Array of GSAP transform and null functions
 const trans = [
-  function skewPlus(target) {
-    let tl = gsap.to(target, {
-      skewX: "8deg",
-      duration: heroDuration
-    });
-    return tl;
-  },
-  function skewMinus(target) {
-    let tl = gsap.to(target, {
-      skewX: "-10deg",
-      duration: heroDuration
-    });
-    return tl;
-  },
-  function rotatePlus(target) {
-    let tl = gsap.to(target, {
-      rotate: "3deg",
-      duration: heroDuration
-    });
-    return tl;
-  },
-  function rotateMinus(target) {
-    let tl = gsap.to(target, {
-      rotate: "-5deg",
-      duration: heroDuration
-    });
-    return tl;
-  },
+	function skewPlus(target) {
+		let tl = gsap.to(target, {
+			skewX: "8deg",
+			duration: heroDuration
+		});
+		return tl;
+	},
+	function skewMinus(target) {
+		let tl = gsap.to(target, {
+			skewX: "-10deg",
+			duration: heroDuration
+		});
+		return tl;
+	},
+	function rotatePlus(target) {
+		let tl = gsap.to(target, {
+			rotate: "3deg",
+			duration: heroDuration
+		});
+		return tl;
+	},
+	function rotateMinus(target) {
+		let tl = gsap.to(target, {
+			rotate: "-5deg",
+			duration: heroDuration
+		});
+		return tl;
+	},
 	(target)=>(null),
 	(target)=>(null),
 	(target)=>(null),
@@ -91,7 +91,7 @@ const trans = [
 
 // Returns random transform
 function getRandomTransform() {
-  return trans[Math.floor(Math.random() * trans.length)];
+	return trans[Math.floor(Math.random() * trans.length)];
 }
 
 // List of all split characters in #hero
@@ -110,74 +110,77 @@ buildScrollTimeline(words);
 
 // Expects array of characters
 function buildCharacterAnimation(chars){
-  // Create new timeline
-  const heroTimeline = gsap.timeline();
-  // For each character...
-  chars.forEach((v, i) => {
-    // Pick random colour
-    let rColor = getRandomColor();
-    // Pick random transform
-    let rTrans = getRandomTransform();
-    // Add transform to timeline at index / 10 seconds
-    heroTimeline.add(rTrans(v), i / 10);
-    // Add color class to character
-    v.classList.add(rColor);
-    // Add same transition & color to corresponding #head character
-    try{
-      headChars[i].classList.add(rColor);
-      heroTimeline.add(rTrans(headChars[i]));
-      // Catch error of index being beyond #head chars
-    } catch {null};
-  });
+	// Create new timeline
+	const heroTimeline = gsap.timeline();
+	// For each character...
+	chars.forEach((v, i) => {
+		// Pick random colour
+		let rColor = getRandomColor();
+		// Pick random transform
+		let rTrans = getRandomTransform();
+		// Add transform to timeline at index / 10 seconds
+		heroTimeline.add(rTrans(v), i / 10);
+		// Add color class to character
+		v.classList.add(rColor);
+		// Add same transition & color to corresponding #head character
+		try{
+			headChars[i].classList.add(rColor);
+			heroTimeline.add(rTrans(headChars[i]));
+			// Catch error of index being beyond #head chars
+		} catch {null};
+	});
 }
 
 // Expects array of words with characters
 function buildScrollTimeline(words) {
-  // Create new timeline
-  const scrollTimeline = gsap.timeline({
-    // Add ScrollTrigger
-    scrollTrigger: {
-      // Scrub animation with scroll in real time
-      scrub: true,
-      // Watch #hero element as trigger
-      trigger: "#hero",
-      // Start timeline when scroll and element are top aligned
-      start: "top",
-      // End timeline when bottom of scroll is 50% down the element
-      end: "bottom +=50%",
-      onLeave: ()=>{
-        head.classList.add("show");
-      },
-      onEnterBack: ()=>{
-        head.classList.remove("show");
-      }
-    }
-  });
-  // Iterate over words
-  words.forEach((word, wordIndex) => {
-    // For each child(character)...
-    word.childNodes.forEach((char, charIndex) => {
-      // Add CharacterScrollAnimation to scrollTimeline
-      scrollTimeline.add(buildCharacterScrollAnimation(char, charIndex, wordIndex));
-    });
-  });
+	// Create new timeline
+	const scrollTimeline = gsap.timeline({
+		// Add ScrollTrigger
+		scrollTrigger: {
+			// Scrub animation with scroll in real time
+			scrub: true,
+			// Watch #hero element as trigger
+			trigger: "#hero",
+			// Start timeline when scroll and element are top aligned
+			start: "top",
+			// End timeline when bottom of scroll is 50% down the element
+			end: "bottom +=50%",
+			// Show header when animation is finished
+			onLeave: ()=>{
+				head.classList.add("show");
+			},
+			// Hide header when hero scrolls back into view
+			onEnterBack: ()=>{
+				head.classList.remove("show");
+			},
+			}
+		}
+	});
+	// Iterate over words
+	words.forEach((word, wordIndex) => {
+		// For each child(character)...
+		word.childNodes.forEach((char, charIndex) => {
+			// Add CharacterScrollAnimation to scrollTimeline
+			scrollTimeline.add(buildCharacterScrollAnimation(char, charIndex, wordIndex));
+		});
+	});
 }
 
 // Expects character node element, index of character in word, index of word in hero
 function buildCharacterScrollAnimation(char,charIndex,wordIndex){
-  // Return gsap animation...
-  return gsap.to(char, {
-    // Transform origin either top right or top left depending on odd/even
-    transformOrigin: `top ${wordIndex % 2 ? "right" : "left"}`,
-    // Scale to nothing
-    scale: 0,
-    // Translate characterIndex * 75% either left or right
-    translateX: `${wordIndex % 2 ? "+" : "-"}${charIndex * 75}%`,
-    // Translate upwards 100% so off screen
-    translateY: "-100%",
-    // Rotate 90deg clock or anti-clock
-    rotate: `${wordIndex % 2 ? "+" : "-"}90deg`
-  });
+	// Return gsap animation...
+	return gsap.to(char, {
+		// Transform origin either top right or top left depending on odd/even
+		transformOrigin: `top ${wordIndex % 2 ? "right" : "left"}`,
+		// Scale to nothing
+		scale: 0,
+		// Translate characterIndex * 75% either left or right
+		translateX: `${wordIndex % 2 ? "+" : "-"}${charIndex * 75}%`,
+		// Translate upwards 100% so off screen
+		translateY: "-100%",
+		// Rotate 90deg clock or anti-clock
+		rotate: `${wordIndex % 2 ? "+" : "-"}90deg`
+	});
 }
 
 // ==================NAV SLIDER==================
